26ed39c906e0f045ce84c8c875c3d10f77823c2b
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-01 17:15:26 -0500

    Added current mod files
    here is my shabby start

---
+++ NeatoJokers/NeatoJokers.lua
@@ -0,0 +1,94 @@
+SMODS.Atlas {
+    key = "NeatoJokers",
+    path = "NeatoJokers.png",
+    px = 71,
+    py = 95,
+}
+
+SMODS.Joker {
+    key = "jokerOTY", 
+    name = "Joker of the Year",
+    loc_txt = {
+        name = "Joker of the Year",
+        text = { "Retriggers all cards",
+                "with a {C:attention}seal{} when played",
+            },
+    }, 
+    unlocked = true, 
+    discovered = true, 
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    config = {extra = { repetitions = 1}},
+    rarity = 3,
+    atlas = "NeatoJokers",
+    pos = {x = 0, y = 0},
+    cost = 7, 
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.repetition and not context.repetition_only then
+            if context.other_card:get_seal() then
+                return {
+                  message = localize('k_again_ex'),
+                  repetitions = card.ability.extra.repetitions,
+                  card = card
+                }
+              end
+            end
+        end
+}
+
+SMODS.Joker {
+    key = "icecreamsandwich", 
+    name = "Ice Cream Sandwich",
+    loc_txt = {
+        name = "Ice Cream Sandwich",
+        text = {"{X:mult,C:white} X#1# {} Mult",
+                "loses {X:mult,C:white} X#2# {} Mult per",
+                "round played"},
+    }, 
+    unlocked = true, 
+    discovered = true, 
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    config = {Xmult = 5, extra = 1},
+    rarity = 2,
+    atlas = "NeatoJokers",
+    pos = {x = 1, y = 0},
+    cost = 5,
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.Xmult, card.ability.extra}}
+      end, 
+    calculate = function(self, card, context)
+    if context.end_of_round and not context.repetition and not context.game_over and not context.blueprint then
+            if card.ability.Xmult - card.ability.extra <= 1 then 
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound('tarot1')
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+                            func = function()
+                                    G.jokers:remove_card(card)
+                                    card:remove()
+                                    card = nil
+                                return true; end})) 
+                        return true
+                    end
+                })) 
+                return {
+                    message = localize('k_eaten_ex'),
+                    colour = G.C.RED
+                }
+            else
+                card.ability.Xmult = card.ability.Xmult - card.ability.extra
+                return {
+                    message = localize{type='variable',key='a_mult_minus',vars={card.ability.extra}},
+                    colour = G.C.RED
+                }
+                end
+            end
+        end
+}
