8d63b96fc85f0eab0764dfa51e1247406d8357a9
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-01 20:49:07 -0500

    Update NeatoJokers.lua
    Added Dogs Playing Balatro, tested and it works!

--- NeatoJokers/NeatoJokers.lua
+++ NeatoJokers/NeatoJokers.lua
@@ -146,7 +146,7 @@ SMODS.Joker {
     end
 } ]]
 
-SMODS.Joker { -- Gonna start just adding these in so we can focus on the calculate functions :D
+SMODS.Joker { -- NeatNote: Thought I'd give this concept a try and it just works! 
     key = "dogsplayingbalatro",
     loc_txt = {
         name = "Dogs Playing Balatro",
@@ -159,12 +159,29 @@ SMODS.Joker { -- Gonna start just adding these in so we can focus on the calcula
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    config = {{extra = 4}},
+    config = {extra = 4},
     rarity = 3,
     atlas = "NeatoJokers",
     pos = { x = 3, y = 0 },
-    cost = 6, -- TODO:  and calculate function
+    cost = 6,
     loc_vars = function(self, info_queue, card)
         return { vars = {''..(G.GAME and G.GAME.probabilities.normal or 1), card.ability.extra}}
     end,
+    calculate = function(self, card, context)
+        if context.after and not context.blueprint then
+        if pseudorandom('dogs') < G.GAME.probabilities.normal/card.ability.extra then
+            G.E_MANAGER:add_event(Event({
+                 func = function() 
+                    local card = create_card('Planet',G.consumeables, nil, nil, nil, nil, nil, 'car')
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                        G.GAME.consumeable_buffer = 0
+                        return true
+                    end
+                }))   
+                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, 
+                {message = localize('k_plus_planet'), colour = G.C.SECONDARY_SET.Planet}) 
+                end
+            end
+        end
 }
