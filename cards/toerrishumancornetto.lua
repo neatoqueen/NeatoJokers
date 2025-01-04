46b8e32206cb3ef0219213f1ad4d918fe59a9755
Amy Hurrell <taliajhurrell@gmail.com>
2025-01-04 20:01:38 +0000

    trigger first two cornettos on each use

--- cards/toerrishumancornetto.lua
+++ cards/toerrishumancornetto.lua
@@ -24,44 +24,48 @@ SMODS.Joker {
     pos = {x = 2, y = 2},
     cost = 5,
     calculate = function(self, card, context)
-        if context.joker_main then
+        if context.scoring_hand then
             local suits = count_suits(context)
-            if suits == card.ability.extra.suits then
-                return {
-                    message = localize{type='variable',key='a_xmult',vars={card.ability.extra.x_mult}},
-                    Xmult_mod = card.ability.extra.x_mult
-                }
-            elseif suits == card.ability.extra.suits_destroy then
-                if card.ability.extra.x_mult - card.ability.extra.x_mult_mod <= 0 then
-                    G.E_MANAGER:add_event(Event({
-                        func = function()
-                            play_sound('tarot1')
-                            card.T.r = -0.2
-                            card:juice_up(0.3, 0.4)
-                            card.states.drag.is = true
-                            card.children.center.pinch.x = true
-                            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                                func = function()
-                                    G.jokers:remove_card(card)
-                                    card:remove()
-                                    card = nil
-                                    return true
-                                end
-                            }))
-                            return true
-                        end
-                    }))
+            if context.joker_main then
+                if suits == card.ability.extra.suits then
+                    return {
+                        message = localize{type='variable',key='a_xmult',vars={card.ability.extra.x_mult}},
+                        Xmult_mod = card.ability.extra.x_mult
+                    }
+                end
+            elseif context.after then
+                if suits == card.ability.extra.suits_destroy then
+                    if card.ability.extra.x_mult - card.ability.extra.x_mult_mod <= 0 then
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                play_sound('tarot1')
+                                card.T.r = -0.2
+                                card:juice_up(0.3, 0.4)
+                                card.states.drag.is = true
+                                card.children.center.pinch.x = true
+                                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+                                    func = function()
+                                        G.jokers:remove_card(card)
+                                        card:remove()
+                                        card = nil
+                                        return true
+                                    end
+                                }))
+                                return true
+                            end
+                        }))
 
-                    return {
-                        message = localize('k_eaten_ex'),
-                        colour = G.C.RED
-                    }
-                else
-                    card.ability.extra.x_mult = card.ability.extra.x_mult - card.ability.extra.x_mult_mod
-                    return {
-                        message = localize{type='variable',key='a_xmult_minus',vars={card.ability.extra.x_mult_mod}},
-                        colour = G.C.RED
-                    }
+                        return {
+                            message = localize('k_eaten_ex'),
+                            colour = G.C.RED
+                        }
+                    else
+                        card.ability.extra.x_mult = card.ability.extra.x_mult - card.ability.extra.x_mult_mod
+                        return {
+                            message = localize{type='variable',key='a_xmult_minus',vars={card.ability.extra.x_mult_mod}},
+                            colour = G.C.RED
+                        }
+                    end
                 end
             end
         end
