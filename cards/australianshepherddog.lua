9d156847b1a58ac18fd5cab89745bd3c408c7a53
Lars <lars.wijntjes@gmail.com>
2025-01-15 16:59:53 +0100

    Balance change: shepherddog is now guaranteed

--- cards/australianshepherddog.lua
+++ cards/australianshepherddog.lua
@@ -2,9 +2,8 @@ SMODS.Joker {
     key = "australianshepherddog",
     loc_txt = {
         name = "Australian Shepherd Dog",
-        text = {"{C:green}#1# in #2#{} chance to create",
-                "a random {C:planet}Planet{} card",
-                "at the start of round",
+        text = {"Create a random {C:planet}Planet{} card",
+                "when {C:attention}Blind{} is selected",
                 "{C:inactive}(Must have room){}"},
     },
     unlocked = true,
@@ -12,34 +11,28 @@ SMODS.Joker {
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    config = {extra = 2},
     rarity = 1,
     atlas = "NeatoJokers",
     pos = { x = 7, y = 1 },
     cost = 4,
-    loc_vars = function(self, info_queue, card)
-        return { vars = {''..(G.GAME and G.GAME.probabilities.normal or 1), card.ability.extra}}
-    end,
     calculate = function(self, card, context)
         local whoami = context.blueprint_card or card
         if context.setting_blind and not whoami.getting_sliced and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            if pseudorandom('dog') < G.GAME.probabilities.normal/card.ability.extra then
-                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        local card = SMODS.create_card{set = "Planet", area = G.consumeables}
-                        card:add_to_deck()  -- is this needed?
-                        G.consumeables:emplace(card)
-                        G.GAME.consumeable_buffer = 0
-                        return true
-                    end
-                }))
-                return {
-                    card = whoami,
-                    message = localize('k_dog'),
-                    colour = G.C.SECONDARY_SET.Planet,
-                }
-            end
+            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    local card = SMODS.create_card{set = "Planet", area = G.consumeables}
+                    card:add_to_deck()  -- is this needed?
+                    G.consumeables:emplace(card)
+                    G.GAME.consumeable_buffer = 0
+                    return true
+                end
+            }))
+            return {
+                card = whoami,
+                message = localize('k_dog'),
+                colour = G.C.SECONDARY_SET.Planet,
+            }
         end
     end
 }
