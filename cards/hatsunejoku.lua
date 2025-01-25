0e6911c7e83699c0603ca0e9fe00550bced222d5
Lars <lars.wijntjes@gmail.com>
2025-01-25 01:17:02 +0100

    Hatsune Joku V3 (surely the last)

--- cards/hatsunejoku.lua
+++ cards/hatsunejoku.lua
@@ -2,14 +2,10 @@ SMODS.Joker {
     key = "hatsunejoku",
     loc_txt = {
         name = "Hatsune Joku",
-        text = {"This Joker gains {C:chips}+#1#{} Chips when",
-                "a {C:clubs}Club{} or {C:spades}Spade{} card is {C:attention}destroyed{}",
-                "{C:inactive}(Currently {C:chips}+#2#{} {C:inactive}Chips)"},
+        text = {"{C:attention}Retrigger{} played {C:attention}9s{}",
+                "an additional {C:attention}two{} times"},
     },
-    config = { extra = {chips = 0, chip_gain = 39} },
-    loc_vars = function( self, info_queue, card )
-        return { vars = { card.ability.extra.chip_gain, card.ability.extra.chips } }
-    end,
+    config = { extra = { repetitions = 2 } },
     unlocked = true,
     discovered = true, 
     blueprint_compat = true,
@@ -20,23 +16,13 @@ SMODS.Joker {
     pos = { x = 5, y = 0 },
     cost = 5,
     calculate = function(self, card, context)
-        if context.joker_main and card.ability.extra.chips > 0 then
-            return {
-                chips = card.ability.extra.chips,
-            }
-        elseif context.remove_playing_cards and not context.blueprint then
-            local increases = 0
-            for _, card in pairs(context.removed) do
-                if card:is_suit("Clubs") or card:is_suit("Spades") then
-                    increases = increases + 1
-                end
-            end
-            if increases > 0 then
-                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_gain * increases
+        if context.cardarea == G.play and context.repetition and not context.repetition_only then
+            if context.other_card:get_id() == 9 then
+                -- TODO: test this
                 return {
-                    message = localize("k_upgrade_ex"),
-                    colour = G.C.CHIPS,
-                    focus = card,
+                    message = localize('k_again_ex'),
+                    repetitions = card.ability.extra.repetitions,
+                    card = card,
                 }
             end
         end
