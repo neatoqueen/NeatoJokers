1095df22842d1cbda1fb4ba333e4791f95cdb28b
Lars <lars.wijntjes@gmail.com>
2025-02-10 04:07:20 +0100

    Wrong card

--- cards/blueyourself.lua
+++ cards/blueyourself.lua
@@ -11,7 +11,7 @@ SMODS.Joker {
         return any_foils()
     end,
     unlocked = true,
-    discovered = true, 
+    discovered = true,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
@@ -19,22 +19,9 @@ SMODS.Joker {
     atlas = "NeatoJokers",
     pos = { x = 0, y = 0 },
     cost = 8,
-    add_to_deck = function(self, card, from_debuff)
-        -- don't tell Neato I put this in
-        if not from_debuff and next(SMODS.find_card(self.key, true)) then
-            -- check if there's already a blueyourself joker present
-            -- apparently returns aren't supported from this context, so use the old-fashioned way
-            card_eval_status_text(card, 'extra', nil, nil, nil, {
-                message = localize('k_dozens'),
-                colour = G.C.BLUE,
-                delay = 2,  -- `delay` is "hold this for X sec"
-            })
-        end
-    end,
     calculate = function(self, card, context)
         if context.retrigger_joker_check and
-                context.other_card.config.center.key ~= self.key and  -- don't retrigger other blueyourself jokers
-                context.other_ret and context.other_ret.jokers and context.other_ret.jokers.was_blueprinted == nil and  -- don't retrigger foil blueprint copying blueyourself
+                context.other_card.config.center.key ~= self.key and context.other_ret.jokers.was_blueprinted == nil and
                 context.other_card.edition and context.other_card.edition.key == "e_foil" then
             -- joker card retriggers using .retrigger_joker_check
             return {
--- cards/sparecutoffs.lua
+++ cards/sparecutoffs.lua
@@ -13,6 +13,18 @@ SMODS.Joker {
     loc_vars = function(self, info_queue, card)
         return { vars = {card.ability.extra.chip_mod, localize('Two Pair', 'poker_hands'), card.ability.extra.chips}}
     end,
+    add_to_deck = function(self, card, from_debuff)
+        -- don't tell Neato I put this in
+        if not from_debuff and next(SMODS.find_card(self.key, true)) then
+            -- check if there's already a blueyourself joker present
+            -- apparently returns aren't supported from this context, so use the old-fashioned way
+            card_eval_status_text(card, 'extra', nil, nil, nil, {
+                message = localize('k_dozens'),
+                colour = G.C.BLUE,
+                delay = 2,  -- `delay` is "hold this for X sec"
+            })
+        end
+    end,
     calculate = function(self, card, context)
         if context.joker_main and card.ability.extra.chips > 0 then
             return {
