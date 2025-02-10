4f3a08f5b3428b4c52bd60be2a826cdd47cf0d60
Lars <lars.wijntjes@gmail.com>
2025-02-10 13:39:04 +0100

    Fix blue yourself for perkeo/burglar/cartomancer

--- cards/blueyourself.lua
+++ cards/blueyourself.lua
@@ -21,15 +21,19 @@ SMODS.Joker {
     cost = 8,
     calculate = function(self, card, context)
         if context.retrigger_joker_check and
-                context.other_card.config.center.key ~= self.key and context.other_ret.jokers.was_blueprinted == nil and
+                context.other_card.config.center.key ~= self.key and  -- don't retrigger other blueyourself jokers
                 context.other_card.edition and context.other_card.edition.key == "e_foil" then
             -- joker card retriggers using .retrigger_joker_check
-            return {
-                message = localize("k_again_ex"),
-                repetitions = card.ability.extra,
-                message_card = context.blueprint_card or card,
-                was_blueprinted = context.blueprint,
-            }
+            if context.other_ret and context.other_ret.jokers and context.other_ret.jokers.was_blueprinted then
+                -- don't retrigger foil blueprint copying blueyourself
+            else
+                return {
+                    message = localize("k_again_ex"),
+                    repetitions = card.ability.extra,
+                    message_card = context.blueprint_card or card,
+                    was_blueprinted = context.blueprint,
+                }
+            end
         elseif context.repetition and not context.repetition_only and 
                 context.other_card and context.other_card.edition and context.other_card.edition.key == "e_foil" then
             -- playing card retriggers using .repetition
